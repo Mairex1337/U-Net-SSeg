6a71ae17438e26879844e639bc3ba0058db4d92f
daandev <daan.groot2@gmail.com>
2025-05-24 18:04:24 +0200

    added api post function, this way user can make prediction

--- src/deployment/api.py
+++ src/deployment/api.py
@@ -1,17 +1,20 @@
-from fastapi import FastAPI, File, UploadFile
-from pydantic import BaseModel
+import sys
+sys.path.append(r'C:\Users\daand\RUG\applied ml\project\U-Net-SSeg')
+from fastapi import FastAPI
+import uvicorn
 
-def load_model():
-    pass
-
-def make_pred():
-    pass
-
-class User(BaseModel):
-    pass
+from src.utils.load_model import load_model
+from src.utils.make_prediction import make_prediction
 
 app = FastAPI()
 
-@app.post("/file")
-async def create_file(file: UploadFile = File(...)):
-    return {"file_name": file.name}
+
+@app.post("/predict")
+async def predict_segmentation():
+    model = load_model()
+    img_dir = 'inference_input/'
+    make_prediction(model, img_dir)
+    return 'succesfull'
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
