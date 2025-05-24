b35acc5257e2b7dc85ac60fed77ffe3ee834ae27
daandev <daan.groot2@gmail.com>
2025-05-24 19:08:57 +0200

    User can now download output from prediction

--- src/deployment/api.py
+++ src/deployment/api.py
@@ -1,6 +1,10 @@
 import sys
 sys.path.append(r'C:\Users\daand\RUG\applied ml\project\U-Net-SSeg')
+
+import os
+
 from fastapi import FastAPI
+from fastapi.responses import FileResponse, RedirectResponse
 import uvicorn
 
 from src.utils.load_model import load_model
@@ -8,13 +12,20 @@ from src.utils.make_prediction import make_prediction
 
 app = FastAPI()
 
+@app.get('/')
+async def root():
+    return RedirectResponse(url="/docs")
 
 @app.post("/predict")
 async def predict_segmentation():
     model = load_model()
     img_dir = 'inference_input/'
     make_prediction(model, img_dir)
-    return 'succesfull'
+    out_dir = 'inference_output/outputs/predictions/00000.png'
+    cwd = os.getcwd()
+    full_path = os.path.join(cwd, out_dir)
+    print(full_path)
+    return FileResponse(full_path, media_type="application/octet-stream", filename=out_dir)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
