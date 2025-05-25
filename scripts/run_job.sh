b1a229c18f0dbff086990772f7bbd87f11a8f195
Mairex1337 <m.lindenburger.1@student.rug.nl>
2025-05-25 15:58:03 +0200

    Fix: copying over if timeout

--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -6,23 +6,21 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=64
 #SBATCH --mem=514404
-#SBATCH --output=$TMPDIR/slurm-%j.out
-#SBATCH --error=$TMPDIR/slurm-%j.err
+#SBATCH --signal=B:12@600
 
 module purge
 module load Python/3.13.1-GCCcore-14.2.0
 source $HOME/venvs/sem-seg/bin/activate
 
-
-
 cp data.tar.gz $TMPDIR/data.tar.gz
 tar xzf $TMPDIR/data.tar.gz -C $TMPDIR/
 
-
-python3 -m scripts.find_batch_size --model unet
-python3 -m scripts.train_ddp --model unet
-python3 -m scripts.eval --model unet --run-id 1
-
+trap 'DEST=/scratch/$USER/u-net/job_${SLURM_JOBID}; mkdir -p $DEST; tar czvf $DEST/outputs_timeout.tar.gz $TMPDIR/outputs' 12
+(
+    python3 -m scripts.find_batch_size --model unet
+    python3 -m scripts.train_ddp --model unet
+    python3 -m scripts.eval --model unet --run-id 1
+) & wait
 
 
 # copy things back
