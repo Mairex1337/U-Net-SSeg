ef3ed791723631171038628539f1c0d1198e934d
Arthur Reuss <arthur.reuss@gmx.de>
2025-05-29 01:57:32 +0200

    deleted old files

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -6,5 +6,3 @@ from .path import (get_best_checkpoint, get_run_dir, read_config, resolve_path,
                    write_config)
 from .timing import Timer
 from .colorize_mask import convert_grayscale_to_colored_mask
-from .handle_deployment import handle_input_inference, handle_output_inference, load_model, make_prediction
-
