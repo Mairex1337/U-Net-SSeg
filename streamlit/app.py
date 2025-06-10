a2d114e87dc89ed88e01cab519ca37719d6b9653
Arthur Reuss <arthur.reuss@gmx.de>
2025-06-10 12:50:53 +0200

    added .env file to keep track of API Base Url, such that when application runs with Docker we can set the env variable in the Dockerfile or composefile

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+from dotenv import load_dotenv
 from utils import configure_layout
 
-
+load_dotenv()
 configure_layout()
 st.markdown(open("streamlit/Instructions.md").read())
--- streamlit/utils/api_client.py
+++ streamlit/utils/api_client.py
@@ -2,7 +2,6 @@ import requests
 from io import BytesIO
 import os
 
-API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000") #TODO: works with local API but not Docker yet. Maybe work with env variables to make it work with both
 #TODO: see if we can do a progress bar by counting entries in temp_input_dir and comparing them with entries in temp_output_dir/color_masks
 
 def upload_image_or_zip(file_bytes: bytes, filename: str) -> BytesIO:
@@ -20,7 +19,7 @@ def upload_image_or_zip(file_bytes: bytes, filename: str) -> BytesIO:
     """
 
     response = requests.post(
-        f"{API_BASE_URL}/predict-image/returns-zip/",
+        f"{os.getenv('API_BASE_URL')}/predict-image/returns-zip/",
         files=[("files", (filename, file_bytes))],
     )
     response.raise_for_status()
@@ -40,7 +39,7 @@ def upload_video(file_bytes: bytes, filename: str) -> BytesIO:
         requests.exceptions.HTTPError: If the API call fails.
     """
     response = requests.post(
-        f"{API_BASE_URL}/predict-video/returns-zip/",
+        f"{os.getenv('API_BASE_URL')}/predict-video/returns-zip/",
         files={"file": (filename, file_bytes, "video/mp4")}
     )
     response.raise_for_status()
