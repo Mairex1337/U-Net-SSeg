c1272c2d394bd7f1c2d46576b18e2d6fb7c0e76c
Mairex1337 <m.lindenburger.1@student.rug.nl>
2025-06-14 23:04:02 +0200

    Feat: job update

--- scripts/run_full_job.sh
+++ scripts/run_full_job.sh
@@ -15,26 +15,26 @@ source $HOME/venvs/sem-seg/bin/activate
 cp data.tar.gz $TMPDIR/data.tar.gz
 tar xzf $TMPDIR/data.tar.gz -C $TMPDIR/
 
-trap 'DEST=/scratch/$USER/u-net/job_${SLURM_JOBID}; mkdir -p $DEST; tar czvf $DEST/outputs_timeout.tar.gz $TMPDIR/outputs' 12
+trap 'DEST=/scratch/$USER/u-net/job_${SLURM_JOBID}; mkdir -p $DEST; find $TMPDIR/outputs -name "*_best.pth" -exec cp --parents {} $DEST \;; find $TMPDIR/outputs -name "*.log" -exec cp --parents {} $DEST \;; find $TMPDIR/outputs -name "cfg.yaml" -exec cp --parents {} $DEST \;' 12
 (
-    python3 -m scripts.run_hyperparam_tuning --model unet --loss weighted_cle
-    python3 -m scripts.train_ddp --model unet --loss weighted_cle
-    python3 -m scripts.eval --model unet --run-id 1
-    python3 -m scripts.train_ddp --model unet --loss OHEMLoss
-    python3 -m scripts.eval --model unet --run-id 2
+    # python3 -m scripts.run_hyperparam_tuning --model unet --loss weighted_cle
+    # python3 -m scripts.train_ddp --model unet --loss weighted_cle
+    # python3 -m scripts.eval --model unet --run-id 1
+    # python3 -m scripts.train_ddp --model unet --loss OHEMLoss
+    # python3 -m scripts.eval --model unet --run-id 2
     python3 -m scripts.train_ddp --model unet --loss mixed_cle_dice
     python3 -m scripts.eval --model unet --run-id 3
     python3 -m scripts.train_ddp --model unet --loss dice
     python3 -m scripts.eval --model unet --run-id 4
 
-    python3 -m scripts.train_ddp --model unet --loss best
-    python3 -m scripts.eval --model unet --run-id 5
-    python3 -m scripts.train_ddp --model unet --loss best
-    python3 -m scripts.eval --model unet --run-id 6
-    python3 -m scripts.train_ddp --model unet --loss best
-    python3 -m scripts.eval --model unet --run-id 7
-    python3 -m scripts.train_ddp --model unet --loss best
-    python3 -m scripts.eval --model unet --run-id 8
+    # python3 -m scripts.train_ddp --model unet --loss best
+    # python3 -m scripts.eval --model unet --run-id 5
+    # python3 -m scripts.train_ddp --model unet --loss best
+    # python3 -m scripts.eval --model unet --run-id 6
+    # python3 -m scripts.train_ddp --model unet --loss best
+    # python3 -m scripts.eval --model unet --run-id 7
+    # python3 -m scripts.train_ddp --model unet --loss best
+    # python3 -m scripts.eval --model unet --run-id 8
 ) & wait
 
 
